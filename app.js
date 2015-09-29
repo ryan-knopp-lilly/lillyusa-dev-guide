cfe8ab28a85ff0a5abb5b608c802e62b02c9a4e4
unknown <C191229@IC1BCCNU337C4PH.AM.LILLY.com>
2015-09-29 10:03:04 -0400

    Avoid port error on Heroku

--- app.js
+++ app.js
@@ -14,6 +14,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.set('port', (process.env.PORT || 5000));
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
