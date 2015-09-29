2c964f02dce0ea383b08588b039262f150d259de
unknown <C191229@IC1BCCNU337C4PH.AM.LILLY.com>
2015-09-29 11:51:52 -0400

    Removed router, updated listener for port

--- app.js
+++ app.js
@@ -4,18 +4,27 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var request = require('request');
+var async = require('async');
+var md = require("node-markdown").Markdown;
+var moment = require("moment");
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var hbs = require('hbs');
+hbs.registerHelper("addClass", function(shouldAdd, value) {
+  if(shouldAdd) return value;
+  else return "";
+});
+
 
 var app = express();
 
 // view engine setup
-app.set('port', (process.env.PORT || 3000));
+app.listen(process.env.PORT || 5000, function() {
+  console.log("listening on " + app.get('port'));
+});
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
-console.log("Running on port " + app.get('port'));
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -25,8 +34,30 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+
+// Get Stuff from Contentful
+var content = require('./content');
+content.init();
+
+
+// GET home page. 
+app.get('/', function(req, res, next) {
+  res.render('index', { title: "Home", menu: content.menu({}), article: "derp" })
+});
+
+// GET Guideline Article
+app.get('/:permalink', function (req, res, next) {
+  var articleContent = content.article(req.params.permalink);
+  res.render(
+    'article', 
+    { 
+      title: articleContent.fields.title, 
+      menu: content.menu(req.params.permalink), 
+      article: md(articleContent.fields.body),
+      revised: moment(articleContent.sys.updatedAt).format("MMMM Do YYYY, h:mm:ss a")
+    });
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
