dd12a800af361e7120dcf350fd484402cf7b8cbb
unknown <C191229@IC1BCCNU337C4PH.AM.LILLY.com>
2015-09-29 11:12:02 -0400

    tweaking port, reverting content request change

--- app.js
+++ app.js
@@ -11,10 +11,11 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.set('port', (process.env.PORT || 3000));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.set('port', (process.env.PORT || 5000));
+console.log("Running on port " + app.get('port'));
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
