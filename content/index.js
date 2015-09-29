5dc1e4022a62aa8f0f16fb0824702c9b5b6c703b
unknown <C191229@IC1BCCNU337C4PH.AM.LILLY.com>
2015-09-29 08:54:57 -0400

    Work please

--- content/index.js
+++ content/index.js
@@ -17,11 +17,12 @@ exports.init = function(){
 	var options = {
 		//host: 'US_Proxy_Indy.xh1.lilly.com',
 		//port: 9000,
+		host: 'US_Proxy_Indy.xh1.lilly.com',
 		path: '/spaces/' + contentfulSpace + '/entries?access_token=' + accessToken + '&content_type=60PxcHAXoAGkQ6E40WOC2E&include=10',
-		method: 'GET', 
+		method: 'GET'/*, 
 		headers: {
 			Host: 'cdn.contentful.com'
-		}
+		}*/
 	};
 	
 	callback = function(response) {
